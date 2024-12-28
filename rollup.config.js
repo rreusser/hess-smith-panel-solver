2fc6e451d8bcc940f4effc49fa449c0ae8696c30
Ricky Reusser <572717+rreusser@users.noreply.github.com>
2024-12-28 14:30:22 -0600

    Move build

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ import { babel } from '@rollup/plugin-babel';
 export default [
   {
     input: 'src/index.js',
-    output: 'www/bundle.min.js',
+    output: 'docs/bundle.min.js',
     format: 'umd',
     name: 'VortexPanelSolver',
     //babelPresets: ['@babel/preset-env'],
